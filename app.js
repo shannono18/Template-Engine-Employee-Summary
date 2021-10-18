034249f17152354ab28dc7cf2b019ecb381ac159
Shannon Rayes <shannono18@gmail.com>
2021-10-17 22:37:56 -0400

    fixed app.js issue

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const Intern = require("./lib/Intern");
 const inquire = require("inquirer");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 
 var teamList = [];
